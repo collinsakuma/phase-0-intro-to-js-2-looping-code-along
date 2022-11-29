471a0df7ad18c19c4a34ecc35cf45540a4632da8
Collin Sakuma <collinmsakuma@gmail.com>
2022-11-29 15:55:42 -0800

    commit tree 875b25b24568afa8c154524491dece16dd3fca98

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++)  {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const newArray = [];
+
+function writeCards(array, strNames) {
+    for (let count = 0; count < array.length; count++) {
+        newArray.push(`Thank you, ${array[count]}, for the wonderful ${strNames} gift!`);
+    }
+
+    return newArray;
+}
+
+//console.log(writeCards(["Bob", "Billy", "Steve"], "Christmas"));
+
+/*
+const gifts = ["teady bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    let i = 0;
+    while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function countDown(someNum) {
+    let i = someNum;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+//countDown(20);
