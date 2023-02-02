cabd369932e8e1102310b4f0354f059461d38c08
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-01 16:58:41 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+}
+
+wrapGifts(gifts);
+
+const newArray = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(numb) {
+    let i = numb;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
